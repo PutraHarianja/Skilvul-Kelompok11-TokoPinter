1eb55fb3948b5f5e2887a53a414988725188b628
wibii21 <noje215@gmail.com>
2021-11-18 22:17:02 +0700

    update category

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,39 @@
-import { React, useContext } from "react";
-import BookItemCard from "./component/book-item-card";
-import { BooksList } from "./component/styled/book-item-card-styled";
-import { BooksContext } from "./context/books-context";
-
-
-
+import { React, useContext } from 'react';
+import BookItemCard from './component/book-item-card';
+import { BooksList } from './component/styled/book-item-card-styled';
+import { BooksContext } from './context/books-context';
 
 function App() {
-
+  <div className="App">
+    <CategoryIconCard iconElement={<fiction />} />
+  </div>;
   const { books, error, loading } = useContext(BooksContext);
 
   if (loading) {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
   }
 
   if (error) {
-    return <h1>Mobile Legend Time</h1>
+    return <h1>Mobile Legend Time</h1>;
   }
 
   return (
     <div>
       <BooksList>
         {books.map((book) => (
-          <BookItemCard book={book} onDescription={() => { console.log("Description") }} onAddToCart={() => { console.log("Added") }} />
+          <BookItemCard
+            book={book}
+            onDescription={() => {
+              console.log('Description');
+            }}
+            onAddToCart={() => {
+              console.log('Added');
+            }}
+          />
         ))}
       </BooksList>
-    </div >
-  )
+    </div>
+  );
 }
 
 export default App;
