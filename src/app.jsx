a864f4e30faa835cb95837c6f72db92ca5e5dc3a
7574 <16017337.fajar.ridwan@gmail.com>
2021-11-19 14:25:04 +0700

    Input router pages

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,13 @@ import { React, useContext } from "react";
 import BookItemCard from "./component/book-item-card";
 import { BooksList } from "./component/styled/book-item-card-styled";
 import { BooksContext } from "./context/books-context";
-
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Book_ID from "./Pages/book_ID";
+import Cart from "./Pages/cart";
+import Not_Found from "./Pages/notFound";
+import Payment from "./Pages/payment";
+import Books from "./Pages/books";
+import Shipping from "./Pages/shipping";
 
 function App() {
 
@@ -29,4 +33,19 @@ function App() {
   )
 }
 
-export default App;
+export default function App2() {
+  return (
+    <Router>
+
+      <Routes>
+        <Route exact path="/app" element={<App />} />
+        <Route exact path="/book/book_ID" element={<Book_ID />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/404" element={<Not_Found />} />
+        <Route exact path="/payment" element={<Payment />} />
+        <Route exact path="/shipping" element={<Shipping />} />
+        <Route exact path="/book" element={<Books />} />
+      </Routes>
+    </Router>
+  );
+}
